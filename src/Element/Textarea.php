df2a8831a8b0c7cf2f982ccf77a4c463d5ff7dbe
Mathieu Decaffmeyer <mdecaffmeyer@gmail.com>
2015-05-09 09:01:55 +0200

    Added Textarea element

---
+++ src/Element/Textarea.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * Mendo Framework
+ *
+ * (c) Mathieu Decaffmeyer <mdecaffmeyer@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Mendo\Form\Element;
+
+/**
+ * @author Mathieu Decaffmeyer <mdecaffmeyer@gmail.com>
+ */
+class Textarea extends AbstractElement
+{
+    private $value;
+
+    public function __construct($name)
+    {
+        parent::__construct($name);
+
+        $this->rules['value'] = 'required|trim';
+    }
+
+    public function __toString()
+    {
+        $string = '<textarea';
+        foreach ($this->attributes as $attribute => $value) {
+            $string .= ' '.$attribute.'="'.$value.'"';
+        }
+
+        return $string.'>'.$this->value."</textarea>\n";
+    }
+
+    public function setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
